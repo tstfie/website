20e41bc93b6e2ef4715ed086f1ec1bbb08776681
tstfie <niklaywiborny@gmail.com>
2026-01-01 19:57:19 +0100

    Updated something I lost track

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  dataset: import.meta.env.SANITY_DATASET,
   apiVersion: "2024-01-01",
   useCdn: true,
 });
