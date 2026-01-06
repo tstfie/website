79841f7dbc228fda46ad1f62679180daf7beb141
tstfie <niklaywiborny@gmail.com>
2026-01-06 10:23:47 +0100

    test designs page

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,6 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  redirects: {
-      '/designs/': '/designs/25-26/periapsis',
-      '/posts/': '/posts/first-post'
-  },
-
   integrations: [sanity(
     { projectId: '366sb9yi',
       dataset: 'production',
